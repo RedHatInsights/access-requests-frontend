51ac96894524c22724fde4dcbd5a8492bda2e2e3
Karel Hala <khala@redhat.com>
2025-10-24 09:59:25 +0200

    Update all dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   stories: [
